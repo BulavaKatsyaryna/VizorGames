b72f2caa3b1c923de2d4c742c6d8d93da6c1fdf6
Булава Кацярына <78858248+BulavaKatsyaryna@users.noreply.github.com>
2022-02-25 03:37:48 +0300

    Added Save button that creates a dialog box for saving the file

--- src/main/java/com/vizor/test/listeners/ButtonDownloadListener.java
+++ src/main/java/com/vizor/test/listeners/ButtonDownloadListener.java
@@ -8,6 +8,7 @@ import java.awt.event.ActionListener;
 public class ButtonDownloadListener extends Component implements ActionListener {
 
     JFileChooser jFileChooser = new JFileChooser();
+    JButton save = new JButton("Save");
 
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
@@ -16,8 +17,20 @@ public class ButtonDownloadListener extends Component implements ActionListener 
                 JFileChooser.DIRECTORIES_ONLY);
         int res = jFileChooser.showOpenDialog(ButtonDownloadListener.this);
         if (res == JFileChooser.APPROVE_OPTION)
-            JOptionPane.showMessageDialog(
-                    ButtonDownloadListener.this,
+            JOptionPane.showMessageDialog(ButtonDownloadListener.this,
                     jFileChooser.getSelectedFile());
+
+        save.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                jFileChooser.setDialogTitle("Saving a file");
+                jFileChooser.setFileSelectionMode(
+                        JFileChooser.FILES_ONLY);
+                int res = jFileChooser.showSaveDialog(ButtonDownloadListener.this);
+                if (res == JFileChooser.APPROVE_OPTION)
+                    JOptionPane.showMessageDialog(ButtonDownloadListener.this, "File saved");
+            }
+        });
     }
 }
